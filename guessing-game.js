ddd98da8a49b64bdef3a6c4950adb35509b9782c
SolaceMotion <SolaceMotion@users.noreply.github.com>
2021-03-03 17:40:27 +0100

    Add guessing-game.js

---
+++ guessing-game.js
@@ -0,0 +1,62 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function guessingGame(min, max) {
+    
+    if (isNaN(min) || isNaN(max)) {
+        throw new ReferenceError("Parameter must be of type 'number'");
+    }
+    
+    const n = Math.floor(Math.random() * max) + min;
+    let attempts = 1;
+
+        rl.setPrompt(`Input an integer from ${min} through ${max}: `);
+        console.log();
+        rl.prompt();
+        rl.on("line", (guess) => {
+            
+            if (guess < n) {
+                console.log("--------\nToo low!\n--------");
+                attempts +=1;
+                rl.prompt();
+    
+            } else if (guess > n) {
+                console.log("--------\nToo high!\n--------");
+                attempts +=1;
+                rl.prompt();
+        
+            } else if (guess == n) {
+                console.log(`\nCongrats, the correct answer was ${n}. You guessed correctly after ${attempts} attempts!\n`);
+                rl.close();
+    
+            } else if (typeof guess == "string") {
+                console.log("------------------------------\nGuess must be of type 'number'\n------------------------------")
+                rl.prompt();
+            }
+        }
+    );
+    
+    rl.on("SIGINT", () => {
+        rl.question("Exit (y or n)", (input) => {
+            if (input.match(/^y(es)?$/i)) {
+                rl.pause();
+            } else {
+                rl.resume();
+                console.log();
+                rl.prompt();
+            }
+        });
+    });
+}
+
+try {
+    guessingGame(1, 100); //Call function with min-and-max values of your choice
+
+} catch (error) {
+    console.error(error)
+    rl.close();
+}
